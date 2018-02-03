7ae897e4de00c1b58125e06764e48d55746ae7b5
Mengmeng Xu <791952204@qq.com>
2018-02-03 23:09:21 +0000

    use properties file to make application stateless

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,3 +1,6 @@
+import java.io.{File, FileInputStream}
+import java.util.Properties
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
@@ -8,14 +11,19 @@ import org.slf4j.LoggerFactory
 object WebServer extends JsonSupport with AppInfoService with LazyLogging{
   def main(args: Array[String]) {
 
+    val prop = new Properties();
+    prop.load(new FileInputStream(new File(System.getProperty("user.dir")+"/src/main/scala/catalina.properties")));
+
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
 
     implicit val executionContext = system.dispatcher
 
     val route = appInfoServiceRoute
+    val host = prop.getProperty("host","0.0.0.0");
+    val port = Integer.valueOf(prop.getProperty("port","8080"));
 
-    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
+    val bindingFuture = Http().bindAndHandle(route, host, port)
 
     val logger = Logger(LoggerFactory.getLogger("my-logger"))
     logger.info(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
